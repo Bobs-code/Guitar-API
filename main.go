d33b1c0cfa6692bfd79bda5821759a19cf7a228d
Bobby Griffin <robertgriffintm@gmail.com>
2023-03-29 15:10:40 -0400

    Establish PostgreSQL DB connection

--- main.go
+++ main.go
@@ -1,21 +1,62 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-func homePage(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Welcome to the Home page!")
-	fmt.Println("Endpoint Hit: homePage")
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "passwordplaceholder"
+	dbname   = "guitars"
+)
+
+type Guitar struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Brand_id    int    `json:"brand_id"`
+	Year        int    `json:"year"`
+	Description string `json:"description"`
+}
+
+type Guitars []Guitar
+
+func getGuitars(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	// quick test
+	guitars := Guitars{
+		Guitar{Id: 1, Name: "Guitar Name"},
+	}
+
+	json.NewEncoder(writer).Encode(guitars)
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/api/v1/Guitars", getGuitars)
+	fmt.Println("Endpoint Hit: All Guitars endpoint")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-	handleRequests()
+	// handleRequests()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s"+" password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("No error and successfully connected")
 }
