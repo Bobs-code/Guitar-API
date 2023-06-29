6409eb4c8a5dd9c25ec62791e0ae2df69a916378
Bobby Griffin <robertgriffintm@gmail.com>
2023-06-29 10:42:10 -0400

    Fix: remove redundant musician path params

--- main.go
+++ main.go
@@ -23,18 +23,18 @@ func handleRequests() {
 	r.Route("/guitars", func(r chi.Router) {
 		r.Get("/", GetAllGuitars)
 		r.Get("/{guitarId}", GetSingleGuitar)
-		r.Put("/guitar/create", NewGuitar)
-		r.Patch("/guitar/update", UpdateGuitar)
-		r.Delete("/guitar/delete/{guitarId}", DeleteGuitar)
+		r.Put("/create", NewGuitar)
+		r.Patch("/update", UpdateGuitar)
+		r.Delete("/delete/{guitarId}", DeleteGuitar)
 	})
 
 	// Routes for Mucisian Resources
-	r.Route("/mucisians", func(r chi.Router) {
+	r.Route("/musicians", func(r chi.Router) {
 		r.Get("/", AllMusicians)
 		r.Get("/{musicianId}", Musician)
-		r.Put("/musicians/create", NewMusician)
-		r.Patch("/musicians/update/{musicianId}", UpdateMusician)
-		r.Delete("/musician/delete{musicianId}", DeleteMusician)
+		r.Put("/create", NewMusician)
+		r.Patch("/update/{musicianId}", UpdateMusician)
+		r.Delete("/delete/{musicianId}", DeleteMusician)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
