6ee1a728839c74e4a60591d88d43d8030e1cfd7d
Bobby Griffin <robertgriffintm@gmail.com>
2023-09-19 09:38:43 -0400

    add: GET route for single guitar resource

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Bobs-code/Guitar-API/controllers"
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,23 @@ func getGuitars(c *gin.Context) {
 	c.JSON(200, guitars)
 }
 
+func singleGuitar(c *gin.Context) {
+
+	urlId := c.Param("id")
+
+	urlIdInt, err := strconv.Atoi(urlId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	guitar, err := controllers.GetSingleGuitar(urlIdInt)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Guitar not found"})
+	}
+
+	c.JSON(http.StatusOK, guitar)
+}
 func homePage(c *gin.Context) {
 	_, err := c.Writer.WriteString("GuitarAPI Project Home Page")
 	if err != nil {
@@ -28,6 +46,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", homePage)
 	r.GET("/guitars", getGuitars)
+	r.GET("/guitars/:id", singleGuitar)
 
 	// nolint:errcheck
 	r.Run("localhost:8080")
